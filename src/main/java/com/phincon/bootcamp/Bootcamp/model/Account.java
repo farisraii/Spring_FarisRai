b6a718373094b425228ec033fd5e466a2f9cebd7
farisraii <farisrai.github@gmail.com>
2023-12-05 13:11:41 +0700

    first commit

---
+++ src/main/java/com/phincon/bootcamp/Bootcamp/model/Account.java
@@ -0,0 +1,33 @@
+package com.phincon.bootcamp.Bootcamp.model;
+
+
+public class Account {
+    public int id;
+    public String name;
+    public double amount;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+}

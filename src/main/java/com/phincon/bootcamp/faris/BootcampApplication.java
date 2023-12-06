bb020fbfb6edcd2a30bf35385a8aa2f040a79ed5
farisraii <farisrai.github@gmail.com>
2023-12-06 18:07:27 +0700

    beres unit test

--- src/main/java/com/phincon/bootcamp/faris/BootcampApplication.java
+++ src/main/java/com/phincon/bootcamp/faris/BootcampApplication.java
@@ -1,17 +1,20 @@
 package com.phincon.bootcamp.faris;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BootcampApplication {
 
-    public static void main(String[] args) {};
+    public static void main(String[] args) {
+        SpringApplication.run(BootcampApplication.class, args);
+    }
 
     // @Override
     // public void run(String... args) throws Exception {
     //     int limit = 10;
     //     for (int i = 1; i <= limit; i++) {
-    //         AccountLatihan acc = new AccountLatihan();
+    //         Account acc = new Account();
     //         acc.setId(i);
     //         System.out.println("id : " + acc.getId());
     //     }
@@ -19,7 +22,7 @@ public class BootcampApplication {
 	// 	int d =0;
 	// 	while (max) {
 	// 		d++;
-	// 		AccountLatihan acc = new AccountLatihan();
+	// 		Account acc = new Account();
 	// 		acc.setId(d);
 	// 		System.out.println("id : " + acc.getId());
 
